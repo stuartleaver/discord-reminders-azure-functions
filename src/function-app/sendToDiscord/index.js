31cc824e0881fa30a3bb10043bcf02c27ceb2339
stuartleaver <stuartleaveruk@gmail.com>
2020-04-30 21:02:09 +0100

    Update Bot for Durable Function
    Made changes to the Bot so that it will call the Durable Feunction rather than the test Function. Also made changes to variable names to make them more consistent.

--- src/function-app/sendToDiscord/index.js
+++ src/function-app/sendToDiscord/index.js
@@ -1,18 +1,5 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an orchestrator function.
- * 
- * Before running this sample, please:
- * - create a Durable orchestration function
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your
- *   function app in Kudu
- */
-
-module.exports = async function (context) {
+﻿module.exports = async function (context) {
     const payload = context.bindings.payload;
 
-    context.log(`You scheduled ${payload.message} to happen now.`)
-
-    return `You scheduled ${message} to happen now.`;
+    return `You scheduled ${payload.reminder} to happen now.`;
 };
