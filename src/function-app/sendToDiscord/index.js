80e5a6ec23c297e9d592704e861e66d0735829e8
stuartleaver <stuartleaveruk@gmail.com>
2020-05-04 20:44:03 +0100

    Add webhook call to the Azure Function
    Added the ability for the sendToDiscord Function to call a webhook to send the reminder back to Discord.

--- src/function-app/sendToDiscord/index.js
+++ src/function-app/sendToDiscord/index.js
@@ -1,5 +1,18 @@
-﻿module.exports = async function (context) {
+﻿const axios = require('axios').default
+
+module.exports = async function (context) {
     const payload = context.bindings.payload;
 
-    return `You scheduled ${payload.reminder} to happen now.`;
+    axios.post(process.env["DiscordWebhook"], {
+        content: `Reminder '${payload.reminder}' was scheduled to happen now.`
+    })
+        .then(function (response) {
+            console.log(`Discord response: ${response.status}`);
+        })
+        .catch(function (error) {
+            console.log(`Error: ${error}`);
+        })
 };
+
+
+
